ce7e16c8ed9fa2f62885777307deee831928c29a
Neryad <dayern.gomez@hotmail.com>
2019-12-09 21:48:50 -0400

    Staring app

---
+++ src/app/models/lista.model.ts
@@ -0,0 +1,20 @@
+import { ListaItem } from './lista-item.mode';
+
+export class Lista{
+
+    id: number;
+    titulo: string;
+    creadaEn: Date;
+    terminadaEn: Date;
+    terminada: boolean;
+    items: ListaItem[];
+
+    constructor( titulo: string) {
+        this.titulo = titulo;
+        this.creadaEn = new Date();
+        this.terminada = false;
+        this.items = [];
+        this.id = new Date().getTime();
+    }
+
+}
